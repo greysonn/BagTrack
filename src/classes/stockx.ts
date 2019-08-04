a5ca417bd5df232b97163ff05dec4bc976200413
R-y-x-n <purkaitryan@gmail.com>
2019-08-04 01:25:01 -0700

    Update stockx.ts

--- src/classes/stockx.ts
+++ src/classes/stockx.ts
@@ -23,9 +23,15 @@ export class Stockx {
         method: 'POST',
         url: 'https://stockx.com/api/login',
         headers: {
-          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-          'content-type': 'application/json'
+          'Host': 'stockx.com',
+          'Connection': 'keep-alive',
+          'Upgrade-Insecure-Requests': 1,
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+          'Accept-Encoding': 'gzip, deflate',
+          'Accept-Language': 'en-US,en;q=0.9,'
         },
+        gzip: true,
         body: {
           email,
           password
